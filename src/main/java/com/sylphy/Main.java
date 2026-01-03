b28cb1548f03c4b9028182253d9b14db3f279386
tyt <2084105638@qq.com>
2026-01-03 15:26:50 +0800

    init

---
+++ src/main/java/com/sylphy/Main.java
@@ -0,0 +1,11 @@
+package com.sylphy;
+
+/**
+ * @author apple
+ * @since 2026/1/3 01:15
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
