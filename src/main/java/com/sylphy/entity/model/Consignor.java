0e6d7ca2ea470c411940f29f67d5654b7d482afa
tyt <2084105638@qq.com>
2026-01-04 11:04:15 +0800

    refactor: 为 User 和 Consignor 实体类添加 MyBatis Plus 注解
    - 添加 @TableName、@TableId、@TableField 注解
    - 主键使用 IdType.ASSIGN_ID 雪花算法
    - 修改 User.type 字段类型从 Boolean 改为 Integer

--- src/main/java/com/sylphy/entity/model/Consignor.java
+++ src/main/java/com/sylphy/entity/model/Consignor.java
@@ -1,36 +1,47 @@
 package com.sylphy.entity.model;
 
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+
+import java.io.Serializable;
 
 /**
  * sys_consignor
  */
 @Data
+@TableName("sys_consignor")
 public class Consignor implements Serializable {
     /**
      * 货主主键 id
      */
+    @TableId(value = "consignor_id", type = IdType.ASSIGN_ID)
     private Long consignorId;
 
     /**
      * 关联的用户 id
      */
+    @TableField("user_id")
     private Long userId;
 
     /**
      * 邮箱
      */
+    @TableField("email")
     private String email;
 
     /**
      * 手机号
      */
+    @TableField("phone")
     private String phone;
 
     /**
      * 密码
      */
+    @TableField("password")
     private String password;
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/sylphy/entity/model/User.java
+++ src/main/java/com/sylphy/entity/model/User.java
@@ -1,22 +1,31 @@
 package com.sylphy.entity.model;
 
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+
+import java.io.Serializable;
 
 /**
  * sys_user
+ * @author apple
  */
 @Data
+@TableName("sys_user")
 public class User implements Serializable {
     /**
      * 用户主键id
      */
+    @TableId(value = "user_id", type = IdType.ASSIGN_ID)
     private Long userId;
 
     /**
      * 用户类型 0:管理员 1:货主 2:调度员 3:司机
      */
-    private Boolean type;
+    @TableField("type")
+    private Integer type;
 
     private static final long serialVersionUID = 1L;
 }
